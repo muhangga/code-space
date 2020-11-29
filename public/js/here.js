49e6353afc2f9f57431bfe49906b322e2f9036a9
muhangga <anggayasdoe11@gmail.com>
2020-11-29 15:10:11 +0700

    read data

--- public/js/here.js
+++ public/js/here.js
@@ -32,74 +32,67 @@ if (navigator.geolocation) {
          let behavior = new H.mapevents.Behavior(mapEvents);
 
          // // Marker
-      function addDragableMarker(map, behavior) {
-         let inputLat = document.getElementById('lat');
-         let inputLng = document.getElementById('lng');
+         function addDragableMarker(map, behavior) {
+            let inputLat = document.getElementById('lat');
+            let inputLng = document.getElementById('lng');
 
-         if (inputLat.value != '' && inputLng.value != '') {
-            objLocalCoord = {
-               lat: inputLat.value,
-               lng: inputLng.value
+            if (inputLat.value != '' && inputLng.value != '') {
+               objLocalCoord = {
+                  lat: inputLat.value,
+                  lng: inputLng.value
+               }
             }
+
+            let marker = new H.map.Marker(objLocalCoord, {
+               volatility: true
+            })
+
+            marker.draggable = true;
+            map.addObject(marker);
+
+            map.addEventListener('dragstart', function (ev) {
+               let target = ev.target,
+                  pointer = ev.currentPointer;
+               if (target instanceof H.map.Marker) {
+                  let targetPosition = map.geoToScreen(target.getGeometry());
+                  target['offset'] = new H.math.Point(
+                     pointer.viewportX - targetPosition.x, pointer.viewportY - targetPosition.y
+                  );
+                  behavior.disable();
+               }
+            }, false);
+
+            map.addEventListener('drag', function (ev) {
+               let target = ev.target,
+                  pointer = ev.currentPointer;
+               if (target instanceof H.map.Marker) {
+                  target.setGeometry(
+                     map.screenToGeo(
+                        pointer.viewportX - target['offset'].x, pointer.viewportY - target['offset'].y
+                     )
+                  );
+               }
+            }, false);
+
+            map.addEventListener('dragend', function (ev) {
+               let target = ev.target;
+               if (target instanceof H.map.Marker) {
+                  behavior.enable();
+                  let resultCoord = map.screenToGeo(
+                     ev.currentPointer.viewportX,
+                     ev.currentPointer.viewportY
+                  );
+                  // console.log(resultCoord)
+                  inputLat.value = resultCoord.lat.toFixed(5);
+                  inputLng.value = resultCoord.lng.toFixed(5);
+               }
+            }, false);
          }
 
-         let marker = new H.map.Marker(objLocalCoord, {
-            volatility: true
-         })
-
-         marker.draggable = true;
-         map.addObject(marker);
-
-         // disable the default draggability of the underlying map
-         // and calculate the offset between mouse and target's position
-         // when starting to drag a marker object:
-         map.addEventListener('dragstart', function (ev) {
-            let target = ev.target,
-               pointer = ev.currentPointer;
-            if (target instanceof H.map.Marker) {
-               let targetPosition = map.geoToScreen(target.getGeometry());
-               target['offset'] = new H.math.Point(
-                  pointer.viewportX - targetPosition.x, pointer.viewportY - targetPosition.y
-               );
-               behavior.disable();
-            }
-         }, false);
-
-         // Listen to the drag event and move the position of the marker
-         // as necessary
-         map.addEventListener('drag', function (ev) {
-            let target = ev.target,
-               pointer = ev.currentPointer;
-            if (target instanceof H.map.Marker) {
-               target.setGeometry(
-                  map.screenToGeo(
-                     pointer.viewportX - target['offset'].x, pointer.viewportY - target['offset'].y
-                  )
-               );
-            }
-         }, false);
-
-         // re-enable the default draggability of the underlying map
-         // when dragging has completed
-         map.addEventListener('dragend', function (ev) {
-            let target = ev.target;
-            if (target instanceof H.map.Marker) {
-               behavior.enable();
-               let resultCoord = map.screenToGeo(
-                  ev.currentPointer.viewportX,
-                  ev.currentPointer.viewportY
-               );
-               // console.log(resultCoord)
-               inputLat.value = resultCoord.lat.toFixed(5);
-               inputLng.value = resultCoord.lng.toFixed(5);
-            }
-         }, false);
-      }
-
-      if (window.action == "submit") {
-         addDragableMarker(map, behavior);
-      }
-   })
+         if (window.action == "submit") {
+            addDragableMarker(map, behavior);
+         }
+      })
 } else {
    console.error("Geolocation is not supported");
 }
